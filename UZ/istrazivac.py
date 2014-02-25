7149c3bc3082160cf82bbd8a5cfa849384b45eb7
vkaracic <karacicvedran@gmail.com>
2014-02-25 17:11:41 +0100

    izmjene

--- UZ/istrazivac.py
+++ UZ/istrazivac.py
@@ -7,7 +7,7 @@ cursor.execute("CALL init")
 conn.commit()
 
 poljeID = int(cursor.execute("SELECT poljeID FROM stanje"))
-cursor.execute("SELECT xkoord, ykoord FROM polje WHERE poljeID = %s;", poljeID)
+cursor.execute("SELECT xkoord, ykoord FROM polje WHERE poljeID = (SELECT poljeID from stanje)")
 xkoord, ykoord = cursor.fetchone()
 
 print xkoord, ykoord
